383894eb362c72f049371d176abbe14f17aad95b
binyam <btechan@gmail.com>
2023-10-24 15:00:29 -0700

    task 6(advanced)

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,37 +7,37 @@
  **/
 void counting_sort(int *array, size_t size)
 {
-    int k, j, *a, *counter;
-    size_t i;
+	int k, j, *a, *counter;
+	size_t i;
 
-    if (array == NULL || size < 2)
-        return;
-    k = array[0];
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] > k)
-            k = array[i];
-    }
-    counter = calloc((k + 1), sizeof(int));
-    for (i = 0; i < size; i++)
-    {
-        counter[array[i]]++;
-    }
-    for (j = 1; j < k; j++)
-    {
-        counter[j + 1] += counter[j];
-    }
-    print_array(counter, k + 1);
-    a = malloc(sizeof(int) * size);
-    for (i = 0; i < size; i++)
-    {
-        counter[array[i]]--;
-        a[counter[array[i]]] = array[i];
-    }
-    for (i = 0; i < size; i++)
-    {
-        array[i] = a[i];
-    }
-    free(a);
-    free(counter);
+	if (array == NULL || size < 2)
+		return;
+	k = array[0];
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > k)
+			k = array[i];
+	}
+	counter = calloc((k + 1), sizeof(int));
+	for (i = 0; i < size; i++)
+	{
+		counter[array[i]]++;
+	}
+	for (j = 1; j < k; j++)
+	{
+		counter[j + 1] += counter[j];
+	}
+	print_array(counter, k + 1);
+	a = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+	{
+		counter[array[i]]--;
+		a[counter[array[i]]] = array[i];
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = a[i];
+	}
+	free(a);
+	free(counter);
 }
