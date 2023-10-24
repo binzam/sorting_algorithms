f2959876f3d18779da660813fd2e0ae5fa240326
binyam <btechan@gmail.com>
2023-10-24 15:56:15 -0700

    task 10(advanced)

--- sort.h
+++ sort.h
@@ -3,7 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#define UP 0
+#define DOWN 1
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -39,7 +40,10 @@ void radix_sort(int *array, size_t size);
 int get_max(int *array, int size);
 void radix_counting_sort(int *array, size_t size, int sig, int *buff);
 void radix_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
-
 #endif
